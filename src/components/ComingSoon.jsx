5e98c36057fec073b9a6d780af5a292a8f48f5a7
Aditya Kumar Singh <adityandmb@gmail.com>
2024-10-02 00:06:00 +0530

    update section

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -6,12 +6,12 @@ import ScrollToTopButton from './ScrollToTopButton';
 const ComingSoon = () => {
   return (
     <div
-      className="h-screen relative bg-cover bg-center flex items-start justify-center"
+      className="h-screen relative bg-cover bg-center flex items-center justify-center"
       style={{ backgroundImage: `url(${comingsoonbanner})` }}
     >
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative bg-neutral-800 p-8 sm:p-16 rounded-lg text-center w-11/12 sm:w-10/12 mt-32 sm:mt-48 h-3/5 sm:h-3/5">
-        <img src={comingsoon} alt="Coming Soon" className="mx-auto mb-5 sm:mb-10 w-40 sm:w-64" />
+      <div className="relative bg-neutral-800 p-6 sm:p-16 rounded-lg text-center w-10/12 sm:w-10/12 h-3/4 sm:h-3/5 flex flex-col justify-center items-center">
+        <img src={comingsoon} alt="Coming Soon" className="mx-auto mb-6 sm:mb-10 w-56 sm:w-64" />
         <h1 className="text-white text-xl sm:text-3xl font-semibold">
           New Fun Events Coming Soon...
         </h1>
--- src/components/PastEvent.jsx
+++ src/components/PastEvent.jsx
@@ -21,27 +21,29 @@ const PastEvent = () => {
   ];
 
   return (
-    <div className="bg-black p-80">
-      <h1 className="text-5xl font-extrabold text-white text-center mb-14">
+    <div className="bg-black p-4 md:p-80 lg:p-80 xl:p-80">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-white text-center mb-10 md:mb-14">
         <span className="text-green-500">Past</span> Events
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:gap-10">
         {events.map((event, index) => (
           <div
             key={index}
-            className="relative rounded-lg p-4" 
+            className="relative rounded-lg p-4"
           >
-            <div className="overflow-hidden rounded-lg border-8 border-green-500 mb-16 transition-shadow duration-300 hover:shadow-[0px_0px_40px_15px_rgba(34,197,94,0.8)]">
+            <div className="overflow-hidden rounded-lg border-4 md:border-8 border-green-500 mb-10 md:mb-16 transition-shadow duration-300 hover:shadow-[0px_0px_40px_15px_rgba(34,197,94,0.8)]">
               <img
                 src={event.img}
                 alt={event.alt}
                 className="w-full h-auto object-cover"
               />
             </div>
-            <div className="absolute bottom-0 left-0 right-0 p-4 flex justify-between items-center bg-gradient-to-t from-black/80 via-black/30 to-transparent">
-              <h3 className="text-white text-lg font-bold">{event.alt}</h3>
-              <button className="bg-green-500 text-black font-bold py-2 px-5 rounded hover:bg-green-600 transition duration-300"> 
-                Get Certified
+            <div className="absolute bottom-0 left-0 right-0 p-2 md:p-4 flex justify-between items-center bg-gradient-to-t from-black/80 via-black/30 to-transparent">
+              <h3 className="text-sm sm:text-base md:text-lg font-bold text-white">
+                {event.alt}
+              </h3>
+              <button className="bg-green-500 text-black font-bold py-1 px-3 sm:py-2 sm:px-4 md:py-2 md:px-5 rounded hover:bg-green-600 transition duration-300">
+                Get Certificate
               </button>
             </div>
           </div>
